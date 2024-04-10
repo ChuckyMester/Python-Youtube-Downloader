5d0a3024aa70bc61247ce9fc1fd902bdede4bc33
ChuckyMester <tamasattila.bodnar@icloud.com>
2024-04-10 14:33:45 +0200

    added only video, only audi, and both option

--- downloader.py
+++ downloader.py
@@ -1,25 +1,30 @@
 from pytube import YouTube
+from moviepy.editor import *
 import os, sys
 
 
 class Downloader():
 
-    def download_video_highest_res(self, url):
+    # Only Video
+    def download_video_highest_res(self, url, output_path):
         try:
-            # Creating the object with the url
             yt = YouTube(url, on_progress_callback=self.progress_function)
-            
-            # Getting the highest resolution as possible
-            video = yt.streams.get_highest_resolution()
-            
-            # Downloading the video
-            video.download()
-            print(f"Video has been downloaded successfully {yt.title}")
+
+            # Choosing the best video only stream
+            video_stream = yt.streams.filter(only_video=True, file_extension='mp4').order_by('resolution').desc().first()
+
+            if output_path:
+                video_stream.download(output_path)
+            else:
+                video_stream.download()
+
+            print(f"Audio has been downloaded successfully {yt.title}")
         except Exception as e:
             print(f"An error has been occured: {e}")
 
 
-    def download_audio(self, url):
+    # Only Audio
+    def download_audio(self, url, output_path):
         try:
             # Creating the object with the url
             yt = YouTube(url, on_progress_callback=self.progress_function)
@@ -28,10 +33,36 @@ class Downloader():
             audio = yt.streams.filter(only_audio=True).first()
             
             # Downloading the audio
-            audio.download()
+            if output_path:
+                audio.download(output_path)
+            else:
+                audio.download()
+
             print(f"Audio has been downloaded successfully {yt.title}")
         except Exception as e:
             print(f"An error has been occured: {e}")
+
+
+    # Video with audio
+    def video_with_audio(self, url, output_path):
+
+        try:
+            # Creating the object with the url
+            yt = YouTube(url, on_progress_callback=self.progress_function)
+            
+            # Getting the highest resolution as possible
+            video = yt.streams.get_highest_resolution()
+            
+            # Downloading the video
+            if output_path:
+                video.download(output_path)
+            else:
+                video.download()
+
+            print(f"Video has been downloaded successfully {yt.title}")
+        except Exception as e:
+            print(f"An error has been occured: {e}")
+
 
 
     def progress_function(self, stream, chunk, bytes_remaining):
@@ -47,3 +78,4 @@ class Downloader():
 
         # Printing the percentage
         print(f'{int(percentage_of_completion)}%')
+        self.percentage = f'{int(percentage_of_completion)}%'
--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from downloader import Downloader
 download = Downloader()
 
 video_link = input('Give me a link:\n')
-choice = input('[A]-Audio   [V]-Video\n')
+output_path = input('Where to download? (default setting is the current folder)')
+choice = input('[A]-Audio   [V]-Video   [B]-Both\n')
 
 choice = choice.lower()
 if choice == 'a':
-    download.download_audio(video_link)
+    download.download_audio(video_link, output_path)
 elif choice == 'v':
-    download.download_video_highest_res(video_link)
+    download.download_video_highest_res(video_link, output_path)
+elif choice == 'b':
+    download.video_with_audio(video_link, output_path)
